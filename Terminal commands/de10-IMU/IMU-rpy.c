17c08ef45b40cc271a51ba5ddc965ff837d4af49
Rahul <rravic19@student.aau.dk>
2020-07-13 16:02:51 +0200

    UPDATE: working IMU rpy values

--- Terminal commands/de10-IMU/IMU-rpy.c
+++ Terminal commands/de10-IMU/IMU-rpy.c
@@ -1,12 +1,23 @@
   #include <stdio.h>
 //#include <string.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 #include <machine/patmos.h>
 #include <stdbool.h>
 //#include "libcorethread/corethread.h"
 //#include "libmp/mp.h"
 #include <math.h>
+#include <time.h>
 
+void delay(int milliseconds)
+{
+    long pause;
+    clock_t now,then;
+
+    pause = milliseconds*(CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while( (now-then) < pause )
+        now = clock();
+}
 
 //LEDs
 #define LED ( *( ( volatile _IODEV unsigned * )	PATMOS_IO_LED ) )
@@ -38,15 +49,14 @@
 #define MPU6050_PWR_MGMT_1         0x6B   // R/W
 #define MPU6050_WHO_AM_I           0x75   // R
 
-int temperature;
-long acc_x, acc_y, acc_z, acc_total_vector[20], acc_av_vector;
-int acc_axis[4], gyro_axis[4];
-double gyro_pitch, gyro_roll, gyro_yaw;
-float angle_roll_acc, angle_pitch_acc, angle_pitch, angle_roll;
-int cal_int, loop_counter;
-double gyro_axis_cal[4];
+int temperature=0;
+float acc_x=0.0, acc_y=0.0, acc_z=0.0, acc_total_vector=0.0;
+int acc_axis[4]={0,0,0,0}, gyro_axis[4]={0,0,0,0};
+double gyro_pitch=0.0, gyro_roll=0.0, gyro_yaw=0.0;
+int cal_int=0, loop_counter=0;
+double gyro_axis_cal[4]={0.0,0.0,0.0,0.0};
 int gyro_address = MPU6050_I2C_ADDRESS;
-bool first_angle;
+bool first_angle=false;
 
 unsigned int signature = 0;
 unsigned int ACCEL_X_H = 0;
@@ -147,29 +157,27 @@ void gyro_signalen(){
 //  if(eeprom_data[28] & 0b10000000)acc_y *= -1;                   //Invert acc_y if the MSB of EEPROM bit 28 is set.
   acc_z = acc_axis[3];                //Set acc_z to the correct axis that was stored in the EEPROM.
 //  if(eeprom_data[30] & 0b10000000)acc_z *= -1;                   //Invert acc_z if the MSB of EEPROM bit 30 is set.
+
+
+   //   printf("-----------------------\n");
+   // printf("ACCEL_X = 0x%.2X%.2X (%d)\n", ACCEL_X_H, ACCEL_X_L, (short int)((ACCEL_X_H << 8) | ACCEL_X_L));
+   // printf("ACCEL_Y = 0x%.2X%.2X (%d)\n", ACCEL_Y_H, ACCEL_Y_L, (short int)((ACCEL_Y_H << 8) | ACCEL_Y_L));
+   // printf("ACCEL_Z = 0x%.2X%.2X (%d)\n", ACCEL_Z_H, ACCEL_Z_L, (short int)((ACCEL_Z_H << 8) | ACCEL_Z_L));
+   // printf("TEMP    = 0x%.2X%.2X (%.1f C)\n", TEMP_H, TEMP_L, ((double)((short int)((TEMP_H << 8) | TEMP_L)) + 12412.0) / 340.0 ); //using datasheet formula for T in degrees Celsius
+   // printf("GYRO_X  = 0x%.2X%.2X (%d)\n", GYRO_X_H, GYRO_X_L, (short int)((GYRO_X_H << 8) | GYRO_X_L));
+   // printf("GYRO_Y  = 0x%.2X%.2X (%d)\n", GYRO_Y_H, GYRO_Y_L, (short int)((GYRO_Y_H << 8) | GYRO_Y_L));
+   // printf("GYRO_Z  = 0x%.2X%.2X (%d)\n", GYRO_Z_H, GYRO_Z_L, (short int)((GYRO_Z_H << 8) | GYRO_Z_L));
 }
 
 
 int main(int argc, char **argv)
 {
   printf("Hello MCU6050!\n");
+  loop_counter = 0;                                                                   //Reset the loop_counter variable.
+  cal_int = 0;
+  first_angle = false;
+  float angle_roll_acc=0.0, angle_pitch_acc=0.0, angle_pitch=0.0, angle_roll=0.0;
   blink_once();
-
-  unsigned int signature = 0;
-  unsigned int ACCEL_X_H = 0;
-  unsigned int ACCEL_X_L = 0;
-  unsigned int ACCEL_Y_H = 0;
-  unsigned int ACCEL_Y_L = 0;
-  unsigned int ACCEL_Z_H = 0;
-  unsigned int ACCEL_Z_L = 0;
-  unsigned int TEMP_L = 0;
-  unsigned int TEMP_H = 0;
-  unsigned int GYRO_X_H = 0;
-  unsigned int GYRO_X_L = 0;
-  unsigned int GYRO_Y_H = 0;
-  unsigned int GYRO_Y_L = 0;
-  unsigned int GYRO_Z_H = 0;
-  unsigned int GYRO_Z_L = 0;
 
   signature = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_WHO_AM_I);
   printf("Signature = 0x%.2X\n", signature);
@@ -180,12 +188,9 @@ int main(int argc, char **argv)
   printf("PWR_MGMT_1 = 0x%.2X\n", i2c_read(MPU6050_I2C_ADDRESS, MPU6050_PWR_MGMT_1));
 
   //for (int i = 0; i < 5; i++) {
-  for (int j=0;j<2;j++) {
+  for (int j=0;j<10;j++) {
   	
-    loop_counter = 0;                                                                   //Reset the loop_counter variable.
-    cal_int = 0;
-    first_angle = false;
-    blink_once();
+    
     
 //    printf("-----------------------\n");
 //    printf("ACCEL_X = 0x%.2X%.2X (%d)\n", ACCEL_X_H, ACCEL_X_L, (short int)((ACCEL_X_H << 8) | ACCEL_X_L));
@@ -200,7 +205,7 @@ int main(int argc, char **argv)
 	//To calculate quadcopter angles.
 	////////////////////////////////////////////////////////////////////////////////////////////
     if(cal_int != 2000){
-      printf("Calibrating the gyro");
+      // printf("Calibrating the gyro");
       //Let's take multiple gyro data samples so we can determine the average gyro offset (calibration).
       for (cal_int = 0; cal_int < 2000 ; cal_int ++){                                   //Take 2000 readings for calibration.
         if(cal_int % 125 == 0){
@@ -217,8 +222,11 @@ int main(int argc, char **argv)
       gyro_axis_cal[1] /= 2000;                                                         //Divide the roll total by 2000.
       gyro_axis_cal[2] /= 2000;                                                         //Divide the pitch total by 2000.
       gyro_axis_cal[3] /= 2000;                                                         //Divide the yaw total by 2000.
+      printf("callibrate done----------------------------------------");
     }
     else{
+      // printf("else loop=======================");
+      delay(1000);
       ///We don't want the esc's to be beeping annoyingly. So let's give them a 1000us puls while calibrating the gyro.
 //      PORTD |= B11110000;                                                               //Set digital poort 4, 5, 6 and 7 high.
 //      delayMicroseconds(1000);                                                          //Wait 1000us.
@@ -235,15 +243,17 @@ int main(int argc, char **argv)
 
       //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
       //In Alterra, 0.000001066 = 0.0000611 * (3.142(PI) / 180degr)
-      angle_pitch -= angle_roll * sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the roll angle to the pitch angel.
-      angle_roll += angle_pitch * sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the pitch angle to the roll angel.
+      angle_pitch -= angle_roll * (float)sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the roll angle to the pitch angel.
+      angle_roll += angle_pitch * (float)sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the pitch angle to the roll angel.
 
       //Accelerometer angle calculations
-      acc_total_vector[0] = sqrt((acc_x*acc_x)+(acc_y*acc_y)+(acc_z*acc_z));           //Calculate the total accelerometer vector.
+
+
+      acc_total_vector = sqrt((acc_x*acc_x)+(acc_y*acc_y)+(acc_z*acc_z));           //Calculate the total accelerometer vector.
 
       //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
-      angle_pitch_acc = asin((float)acc_y/acc_total_vector[0])* 57.296;                //Calculate the pitch angle.
-      angle_roll_acc = asin((float)acc_x/acc_total_vector[0])* -57.296;                //Calculate the roll angle.
+      angle_pitch_acc = asin((float)acc_y/(float)acc_total_vector)* 57.296;                //Calculate the pitch angle.
+      angle_roll_acc = asin((float)acc_x/(float)acc_total_vector)* -57.296;                //Calculate the roll angle.
       
       if(!first_angle){
         angle_pitch = angle_pitch_acc;                                                 //Set the pitch angle to the accelerometer angle.
@@ -256,16 +266,15 @@ int main(int argc, char **argv)
       }
 
       //We can't print all the data at once. This takes to long and the angular readings will be off.
-      if(loop_counter == 0)printf("Pitch: ");
-      if(loop_counter == 1)printf("%f",angle_pitch);
-      if(loop_counter == 2)printf(" Roll: ");
-      if(loop_counter == 3)printf("%f",angle_roll);
-      if(loop_counter == 4)printf(" Yaw: ");
-      if(loop_counter == 5)printf("%f",gyro_yaw / 65.5);
+      // if(loop_counter == 0)printf("Pitch: ");
+      // if(loop_counter == 1)printf("%f",angle_pitch);
+      // if(loop_counter == 2)printf(" Roll: ");
+      // if(loop_counter == 3)printf("%f",angle_roll);
+      // if(loop_counter == 4)printf(" Yaw: ");
+      // if(loop_counter == 5)printf("%f",gyro_yaw / 65.5);
+      printf("Pitch: %f       Roll: %f      Yaw: %f \n", angle_pitch, angle_roll, gyro_yaw/65.5);
       printf("\n");
-
-      loop_counter ++;
-      if(loop_counter == 60)loop_counter = 0;      
+ 
 	  }
   }
 
