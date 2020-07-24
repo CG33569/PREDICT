8208d91d311c66008282d2af82f60f7c1f604e8f
Rahul <rravic19@student.aau.dk>
2020-07-24 08:41:52 +0200

    Uppdate: autonomous flight ontroller code

--- c_apps/de10-IMU/IMU-rpy.c
+++ c_apps/de10-IMU/IMU-rpy.c
@@ -1,6 +1,7 @@
-  #include <stdio.h>
+#include <stdio.h>
 //#include <string.h>
 #include <stdlib.h>
+#include <machine/rtc.h>
 #include <machine/patmos.h>
 #include <stdbool.h>
 //#include "libcorethread/corethread.h"
@@ -48,7 +49,11 @@ void delay(int milliseconds)
 #define MPU6050_GYRO_ZOUT_L        0x48   // R
 #define MPU6050_PWR_MGMT_1         0x6B   // R/W
 #define MPU6050_WHO_AM_I           0x75   // R
+#define MPU6050_GYRO_CONFIG        0x1B   // R
+#define MPU6050_ACCEL_CONFIG       0x1C   // R
+#define MPU6050_CONFIG_REG         0x1A   // R
 
+unsigned long loop_timer;
 int temperature=0;
 float acc_x=0.0, acc_y=0.0, acc_z=0.0, acc_total_vector=0.0;
 int acc_axis[4]={0,0,0,0}, gyro_axis[4]={0,0,0,0};
@@ -117,8 +122,8 @@ void LED_out(int i){
 void gyro_signalen(){
   //Read the MPU-6050
 	ACCEL_X_H = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_XOUT_H);
-	ACCEL_Y_H = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_YOUT_H);
 	ACCEL_X_L = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_XOUT_L);
+	ACCEL_Y_H = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_YOUT_H);
 	ACCEL_Y_L = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_YOUT_L);
 	ACCEL_Z_H = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_ZOUT_H);
 	ACCEL_Z_L = i2c_read(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_ZOUT_L);
@@ -147,16 +152,16 @@ void gyro_signalen(){
   gyro_roll = gyro_axis[1];           //Set gyro_roll to the correct axis that was stored in the EEPROM.
 //  if(eeprom_data[28] & 0b10000000)gyro_roll *= -1;               //Invert gyro_roll if the MSB of EEPROM bit 28 is set.
   gyro_pitch = gyro_axis[2];          //Set gyro_pitch to the correct axis that was stored in the EEPROM.
-//  if(eeprom_data[29] & 0b10000000)gyro_pitch *= -1;              //Invert gyro_pitch if the MSB of EEPROM bit 29 is set.
+  gyro_pitch *= -1;              //Invert gyro_pitch if the MSB of EEPROM bit 29 is set.
   gyro_yaw = gyro_axis[3];            //Set gyro_yaw to the correct axis that was stored in the EEPROM.
-//  if(eeprom_data[30] & 0b10000000)gyro_yaw *= -1;                //Invert gyro_yaw if the MSB of EEPROM bit 30 is set.
+  gyro_yaw *= -1;                //Invert gyro_yaw if the MSB of EEPROM bit 30 is set.
 
-  acc_x = acc_axis[1];                //Set acc_x to the correct axis that was stored in the EEPROM.
-//  if(eeprom_data[29] & 0b10000000)acc_x *= -1;                   //Invert acc_x if the MSB of EEPROM bit 29 is set.
-  acc_y = acc_axis[2];                //Set acc_y to the correct axis that was stored in the EEPROM.
+  acc_x = acc_axis[2];                //Set acc_x to the correct axis that was stored in the EEPROM.
+  acc_x *= -1;                   //Invert acc_x if the MSB of EEPROM bit 29 is set.
+  acc_y = acc_axis[1];                //Set acc_y to the correct axis that was stored in the EEPROM.
 //  if(eeprom_data[28] & 0b10000000)acc_y *= -1;                   //Invert acc_y if the MSB of EEPROM bit 28 is set.
   acc_z = acc_axis[3];                //Set acc_z to the correct axis that was stored in the EEPROM.
-//  if(eeprom_data[30] & 0b10000000)acc_z *= -1;                   //Invert acc_z if the MSB of EEPROM bit 30 is set.
+  acc_z *= -1;                   //Invert acc_z if the MSB of EEPROM bit 30 is set.
 
 
    //   printf("-----------------------\n");
@@ -168,6 +173,16 @@ void gyro_signalen(){
    // printf("GYRO_Y  = 0x%.2X%.2X (%d)\n", GYRO_Y_H, GYRO_Y_L, (short int)((GYRO_Y_H << 8) | GYRO_Y_L));
    // printf("GYRO_Z  = 0x%.2X%.2X (%d)\n", GYRO_Z_H, GYRO_Z_L, (short int)((GYRO_Z_H << 8) | GYRO_Z_L));
 }
+
+void set_gyro_registers()
+{
+  //Setup the MPU-6050
+  i2c_write(MPU6050_I2C_ADDRESS, MPU6050_PWR_MGMT_1, 0x00);                    //Set the register bits as 00000000 to activate the gyro
+  i2c_write(MPU6050_I2C_ADDRESS, MPU6050_GYRO_CONFIG, 0x08);                   //Set the register bits as 00001000 (500dps full scale)
+  i2c_write(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_CONFIG, 0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
+  i2c_write(MPU6050_I2C_ADDRESS, MPU6050_CONFIG_REG, 0x03);                    //Set the register bits as 00000011 (Set Digital Low Pass Filter to ~43Hz)
+}
+
 
 
 int main(int argc, char **argv)
@@ -184,101 +199,106 @@ int main(int argc, char **argv)
 
   printf("PWR_MGMT_1 = 0x%.2X\n", i2c_read(MPU6050_I2C_ADDRESS, MPU6050_PWR_MGMT_1));
   printf("Getting MPU-6050 out of sleep mode.\n");
-  i2c_write(MPU6050_I2C_ADDRESS, MPU6050_PWR_MGMT_1, 0x00);
-  printf("PWR_MGMT_1 = 0x%.2X\n", i2c_read(MPU6050_I2C_ADDRESS, MPU6050_PWR_MGMT_1));
 
+  set_gyro_registers();
+  // i2c_write(MPU6050_I2C_ADDRESS, MPU6050_PWR_MGMT_1, 0x00);
+  // i2c_write(MPU6050_I2C_ADDRESS, MPU6050_GYRO_CONFIG, 0x08);
+  // i2c_write(MPU6050_I2C_ADDRESS, MPU6050_ACCEL_CONFIG, 0x10);
+  // i2c_write(MPU6050_I2C_ADDRESS, MPU6050_CONFIG_REG, 0x03);
+  // printf("PWR_MGMT_1 = 0x%.2X\n", i2c_read(MPU6050_I2C_ADDRESS, MPU6050_PWR_MGMT_1));
+
+  loop_timer = get_cpu_usecs();
   //for (int i = 0; i < 5; i++) {
-  for (int j=0;j<10;j++) {
-  	
-    
-    
-//    printf("-----------------------\n");
-//    printf("ACCEL_X = 0x%.2X%.2X (%d)\n", ACCEL_X_H, ACCEL_X_L, (short int)((ACCEL_X_H << 8) | ACCEL_X_L));
-//    printf("ACCEL_Y = 0x%.2X%.2X (%d)\n", ACCEL_Y_H, ACCEL_Y_L, (short int)((ACCEL_Y_H << 8) | ACCEL_Y_L));
-//    printf("ACCEL_Z = 0x%.2X%.2X (%d)\n", ACCEL_Z_H, ACCEL_Z_L, (short int)((ACCEL_Z_H << 8) | ACCEL_Z_L));
-//    printf("TEMP    = 0x%.2X%.2X (%.1f C)\n", TEMP_H, TEMP_L, ((double)((short int)((TEMP_H << 8) | TEMP_L)) + 12412.0) / 340.0 ); //using datasheet formula for T in degrees Celsius
-//    printf("GYRO_X  = 0x%.2X%.2X (%d)\n", GYRO_X_H, GYRO_X_L, (short int)((GYRO_X_H << 8) | GYRO_X_L));
-//    printf("GYRO_Y  = 0x%.2X%.2X (%d)\n", GYRO_Y_H, GYRO_Y_L, (short int)((GYRO_Y_H << 8) | GYRO_Y_L));
-//    printf("GYRO_Z  = 0x%.2X%.2X (%d)\n", GYRO_Z_H, GYRO_Z_L, (short int)((GYRO_Z_H << 8) | GYRO_Z_L));
-    
-	///////////////////////////////////////////////////////////////////////////////////////////
-	//To calculate quadcopter angles.
-	////////////////////////////////////////////////////////////////////////////////////////////
-    if(cal_int != 2000){
-      // printf("Calibrating the gyro");
-      //Let's take multiple gyro data samples so we can determine the average gyro offset (calibration).
-      for (cal_int = 0; cal_int < 2000 ; cal_int ++){                                   //Take 2000 readings for calibration.
-        if(cal_int % 125 == 0){
-          LED_out(1);   //Change the led status to indicate calibration.
-          printf(".");
+  for (int j=0;j<600;j++)
+  // while(1)
+  {
+    while(loop_timer + 4000 > get_cpu_usecs());                                                  //Start the pulse after 4000 micro seconds.
+    loop_timer = get_cpu_usecs();                                                                //Reset the zero timer.
+
+    printf("loop_timer:  %d\n", loop_timer);
+    delay(100);
+  	///////////////////////////////////////////////////////////////////////////////////////////
+  	//To calculate quadcopter angles.
+  	////////////////////////////////////////////////////////////////////////////////////////////
+      if(cal_int != 2000)
+      {
+        // printf("Calibrating the gyro");
+        //Let's take multiple gyro data samples so we can determine the average gyro offset (calibration).
+        for (cal_int = 0; cal_int < 2000 ; cal_int ++)
+        {                                   //Take 2000 readings for calibration.
+          if(cal_int % 125 == 0)
+          {
+            LED_out(1);   //Change the led status to indicate calibration.
+            printf(".");
+          }
+          gyro_signalen();                                                                //Read the gyro output.
+          gyro_axis_cal[1] += gyro_axis[1];                                               //Ad roll value to gyro_roll_cal.
+          gyro_axis_cal[2] += gyro_axis[2];                                               //Ad pitch value to gyro_pitch_cal.
+          gyro_axis_cal[3] += gyro_axis[3];                                               //Ad yaw value to gyro_yaw_cal.
         }
-        gyro_signalen();                                                                //Read the gyro output.
-        gyro_axis_cal[1] += gyro_axis[1];                                               //Ad roll value to gyro_roll_cal.
-        gyro_axis_cal[2] += gyro_axis[2];                                               //Ad pitch value to gyro_pitch_cal.
-        gyro_axis_cal[3] += gyro_axis[3];                                               //Ad yaw value to gyro_yaw_cal.
+        printf(".");
+        //Now that we have 2000 measures, we need to devide by 2000 to get the average gyro offset.
+        gyro_axis_cal[1] /= 2000;                                                         //Divide the roll total by 2000.
+        gyro_axis_cal[2] /= 2000;                                                         //Divide the pitch total by 2000.
+        gyro_axis_cal[3] /= 2000;                                                         //Divide the yaw total by 2000.
+        printf("callibrate done----------------------------------------");
       }
-      printf(".");
-      //Now that we have 2000 measures, we need to devide by 2000 to get the average gyro offset.
-      gyro_axis_cal[1] /= 2000;                                                         //Divide the roll total by 2000.
-      gyro_axis_cal[2] /= 2000;                                                         //Divide the pitch total by 2000.
-      gyro_axis_cal[3] /= 2000;                                                         //Divide the yaw total by 2000.
-      printf("callibrate done----------------------------------------");
-    }
-    else{
-      // printf("else loop=======================");
-      delay(1000);
-      ///We don't want the esc's to be beeping annoyingly. So let's give them a 1000us puls while calibrating the gyro.
-//      PORTD |= B11110000;                                                               //Set digital poort 4, 5, 6 and 7 high.
-//      delayMicroseconds(1000);                                                          //Wait 1000us.
-//      PORTD &= B00001111;                                                               //Set digital poort 4, 5, 6 and 7 low.
+      else
+      {
+        // printf("else loop=======================");
+        // delay(500);
+        //Let's get the current gyro data.
+        gyro_signalen();
 
-      //Let's get the current gyro data.
-      gyro_signalen();
-
-      //Gyro angle calculations
-      //0.0000611 = 1 / (250Hz / 65.5)
-      //In Alterra,  0.0000611 = 1 / (250Hz / 65.5)
-      angle_pitch += gyro_pitch * 0.0000611;                                           //Calculate the traveled pitch angle and add this to the angle_pitch variable.
-      angle_roll += gyro_roll * 0.0000611;                                             //Calculate the traveled roll angle and add this to the angle_roll variable.
-
-      //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
-      //In Alterra, 0.000001066 = 0.0000611 * (3.142(PI) / 180degr)
-      angle_pitch -= angle_roll * (float)sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the roll angle to the pitch angel.
-      angle_roll += angle_pitch * (float)sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the pitch angle to the roll angel.
-
-      //Accelerometer angle calculations
+        //Gyro angle calculations
+        //0.0000611 = 1 / (250Hz / 65.5)
+        //In Alterra,  0.0000611 = 1 / (250Hz / 65.5)
 
 
-      acc_total_vector = sqrt((acc_x*acc_x)+(acc_y*acc_y)+(acc_z*acc_z));           //Calculate the total accelerometer vector.
 
-      //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
-      angle_pitch_acc = asin((float)acc_y/(float)acc_total_vector)* 57.296;                //Calculate the pitch angle.
-      angle_roll_acc = asin((float)acc_x/(float)acc_total_vector)* -57.296;                //Calculate the roll angle.
-      
-      if(!first_angle){
-        angle_pitch = angle_pitch_acc;                                                 //Set the pitch angle to the accelerometer angle.
-        angle_roll = angle_roll_acc;                                                   //Set the roll angle to the accelerometer angle.
-        first_angle = true;
+
+        angle_pitch += gyro_pitch * 0.0000611;                                           //Calculate the traveled pitch angle and add this to the angle_pitch variable.
+        angle_roll += gyro_roll * 0.0000611;                                             //Calculate the traveled roll angle and add this to the angle_roll variable.
+
+        //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
+        angle_pitch -= angle_roll * (float)sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the roll angle to the pitch angel.
+        angle_roll += angle_pitch * (float)sin(gyro_yaw * 0.000001066);                         //If the IMU has yawed transfer the pitch angle to the roll angel.
+
+        //Accelerometer angle calculations
+
+
+        // acc_total_vector = sqrt((acc_x*acc_x)+(acc_y*acc_y)+(acc_z*acc_z));           //Calculate the total accelerometer vector.
+
+        // //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
+        // angle_pitch_acc = asin(acc_y/acc_total_vector)* 57.296;                //Calculate the pitch angle.
+        // angle_roll_acc = asin(acc_x/acc_total_vector)* -57.296;                //Calculate the roll angle.
+        
+        // if(!first_angle)
+        // {
+        //   angle_pitch = angle_pitch_acc;                                                 //Set the pitch angle to the accelerometer angle.
+        //   angle_roll = angle_roll_acc;                                                   //Set the roll angle to the accelerometer angle.
+        //   first_angle = true;
+        // }
+        // else
+        // {
+        //   angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;                 //Correct the drift of the gyro pitch angle with the accelerometer pitch angle.
+        //   angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;                    //Correct the drift of the gyro roll angle with the accelerometer roll angle.
+        // }
+
+
+
+        printf("Pitch: %f       Roll: %f      Yaw: %f \n", angle_pitch, angle_roll, gyro_yaw/65.5);
+        // printf("Pitch: %f       Roll: %f      Yaw: %f \n", gyro_pitch, gyro_roll, gyro_yaw);
+        // if(loop_counter == 0)Serial.printf("Pitch: ");
+        // if(loop_counter == 1)Serial.printf(angle_pitch ,0);
+        // if(loop_counter == 2)Serial.printf(" Roll: ");
+        // if(loop_counter == 3)Serial.printf(angle_roll ,0);
+        // if(loop_counter == 4)Serial.printf(" Yaw: ");
+        // if(loop_counter == 5)Serial.print(gyro_yaw / 65.5 ,0);
+        printf("\n");
+
       }
-      else{
-        angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;                 //Correct the drift of the gyro pitch angle with the accelerometer pitch angle.
-        angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;                    //Correct the drift of the gyro roll angle with the accelerometer roll angle.
-      }
-
-      //We can't print all the data at once. This takes to long and the angular readings will be off.
-      // if(loop_counter == 0)printf("Pitch: ");
-      // if(loop_counter == 1)printf("%f",angle_pitch);
-      // if(loop_counter == 2)printf(" Roll: ");
-      // if(loop_counter == 3)printf("%f",angle_roll);
-      // if(loop_counter == 4)printf(" Yaw: ");
-      // if(loop_counter == 5)printf("%f",gyro_yaw / 65.5);
-      printf("Pitch: %f       Roll: %f      Yaw: %f \n", angle_pitch, angle_roll, gyro_yaw/65.5);
-      printf("\n");
- 
-	  }
   }
-
-
   return 0;
 }
   
